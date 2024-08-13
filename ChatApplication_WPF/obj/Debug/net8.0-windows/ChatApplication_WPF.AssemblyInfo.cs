6ce8a44cc4bdd08290542b51f53ed1a85efe3127
Pierre Joubert <joubertpierre96@gmail.com>
2024-08-13 12:04:39 +0200

    Cloned repository.

--- ChatApplication_WPF/obj/Debug/net8.0-windows/ChatApplication_WPF.AssemblyInfo.cs
+++ ChatApplication_WPF/obj/Debug/net8.0-windows/ChatApplication_WPF.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChatApplication_WPF")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b03573509b94a6b085de58a6057edb7d4159f1d6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41876021f77081404944df69a7aad7e0b87ef3e7")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChatApplication_WPF")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChatApplication_WPF")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
